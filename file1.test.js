14db83510c5f0e02f132b046e62b82e774e5a974
martin <martin@techstarter.de>
2023-11-20 02:12:23 +0100

    linter

--- file1.test.js
+++
@@ -1,4 +0,0 @@
-const greet=require('./file1');
-test('greets the world',()=>{
-expect(greet("World")).toBe("Hello, World!");
-});
--- file2.test.js
+++
@@ -1,5 +0,0 @@
-const add= require('./file2');
-
-test('adds two numbers',() =>{
- expect(add(2, 3)).toBe(5) ;
-});
